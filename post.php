f5efe4172d6f75dc5b4677f32bf7af6fc13e6083
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-09-18 11:06:42 -0500

    Update post.php

--- post.php
+++ post.php
@@ -3,7 +3,7 @@ $pageTitle ="Post";
 include "view-header.php";
 ?>
 <h1> Post </h1>
-<form>
+<form method="post" action="post-result.php">
   <input type = "text" name= "my-name">
   <input type = "submit" value = "Post Data">
 </form>
